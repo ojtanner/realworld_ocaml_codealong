d6ceda4aeeebe2ee774d98cc8d68c782cbd58653
Oliver Julian Taner <oliver-tanner@outlook.com>
2020-02-01 20:12:03 +0100

    Write code snippets till page 42

--- 02_variables_and_functions/functions.ml
+++ 02_variables_and_functions/functions.ml
@@ -70,3 +70,79 @@ let add x y =
 let add_2 = add 2;;
 
 let four = add_2 2;;
+
+(*
+ * Recursive functions:
+ * If you want to define a recursive function,
+ * you mark it with "rec"
+ *)
+let rec range a b f =
+  match a - b with
+  | 0 -> ()
+  | _ ->
+      f a;
+      range (a + 1) b f;;
+
+(*
+ * "function" keyword:
+ * You can declare functions using the
+ * "function" keyword.
+ * Why and when?
+ * Whenever you need to pattern match
+ * your input argument.
+ *)
+let f = function
+  | [] -> None
+  | hd :: tl -> Some hd
+(* Here we directly pattern match the
+ * list provided as input argument
+ * thanks to the "function" keyword.
+ *)
+
+(*
+ * Labeled Parameters:
+ * You can label parameters.
+ * Why would you do that?
+ * Because it can sometimes be
+ * more explicit what is being passed to
+ * the function.
+ * Also, labeled arguments can be passed
+ * in any order.
+ *)
+let my_function ~dividend ~divisor = dividend / divisor;;
+
+(* Invoking a function with labeled parameters: *)
+my_function ~dividend:10 ~divisor:2
+
+(* You can also use shorthands if there
+ * already exist variables with the same
+ * names as your labeled parameters.
+ *)
+let dividend = 10;;
+let divisor = 2;;
+
+my_function ~dividend ~divisor;;
+
+(* Gotchas with labeled arguments:
+ * If you pass a function with
+ * labeled arguments to another function,
+ * you have to be VERY careful to
+ * invoke the function with labeled
+ * arguments in such a way that
+ * the labeled arguments are passed
+ * in the same order as in the
+ * function signature.
+ *)
+
+let wrapper f divisor dividend = f ~dividend ~divisor;;
+
+wrapper my_function 10 20
+
+(* The code above works.
+ * The code below not.
+ *)
+
+(*
+ * let wrapper f divisor dividend = f ~divisor ~dividend;;
+ * wrapper my_function 10 20
+ *)
